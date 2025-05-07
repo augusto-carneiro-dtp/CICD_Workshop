c4cc61f72a6d144a11099a55ced1f72fe05ae97c
augusto.carneiro <augusto.carneiro@dataprev.gov.br>
2025-05-07 17:11:46 +0000

    Implemented CI pipeline

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -2,7 +2,10 @@ from constructs import Construct
 from aws_cdk import (
     Stack,
     CfnOutput,
-    aws_codecommit as codecommit,
+    aws_codeconnections as codeconnections,
+    aws_codepipeline as codepipeline,
+    aws_codebuild as codebuild,
+    aws_codepipeline_actions as codepipeline_actions,
 )
 
 class PipelineCdkStack(Stack):
@@ -10,14 +13,65 @@ class PipelineCdkStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Creates a CodeCommit repository called 'CICD_Workshop'
-        repo = codecommit.Repository(
-            self, 'CICD_Workshop',
-            repository_name = 'CICD_Workshop',
-            description = 'Repository for my application code and infrastructure'
+        # Creates a CodeConnections resource called 'CICD_Workshop_Connection'
+        SourceConnection = codeconnections.CfnConnection(self, "CICD_Workshop",
+                connection_name="CICD_Workshop_Connection",
+                provider_type="GitHub",
+        )
+
+        pipeline = codepipeline.Pipeline(
+            self, 'CICD_Pipeline',
+            cross_account_keys = False,
+            pipeline_type=codepipeline.PipelineType.V2,
+            execution_mode=codepipeline.ExecutionMode.QUEUED
+        )
+
+        code_quality_build = codebuild.PipelineProject(
+            self, 'CodeBuild',
+            build_spec = codebuild.BuildSpec.from_source_filename('./buildspec_test.yml'),
+            environment = codebuild.BuildEnvironment(
+                build_image = codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged = True,
+                compute_type = codebuild.ComputeType.LARGE
+            ),
+        )
+
+        source_output = codepipeline.Artifact()
+        unit_test_output = codepipeline.Artifact()
+
+        source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
+          action_name = 'GitHub',
+          owner = "augusto-carneiro-dtp",
+          repo = "cicd-workshop",
+          output = source_output,
+          branch = "main",
+          trigger_on_push = True,
+          connection_arn = "arn:aws:codeconnections:us-east-2:309164021931:connection/fa295191-6f00-407a-b223-f2ae42779e92"
+        )
+
+        pipeline.add_stage(
+          stage_name = 'Source',
+          actions = [source_action]
+        )
+
+        build_action = codepipeline_actions.CodeBuildAction(
+            action_name = 'Unit-Test',
+            project = code_quality_build,
+            input = source_output,  # The build action must use the CodeStarConnectionsSourceAction output as input.
+            outputs = [unit_test_output]
+        )
+
+        pipeline.add_stage(
+            stage_name = 'Code-Quality-Testing',
+            actions = [build_action]
         )
 
         CfnOutput(
-            self, 'CodeCommitRepositoryUrl',
-            value = repo.repository_clone_url_grc
+            self, 'SourceConnectionArn',
+            value = SourceConnection.attr_connection_arn
+        )
+
+        CfnOutput(
+            self, 'SourceConnectionStatus',
+            value = SourceConnection.attr_connection_status
         )
